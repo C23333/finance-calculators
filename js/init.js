517c7ec7d4167e00eb51bc465f6ddb1c93308bad
liuyongkai <liuyongkai@hotmail.com>
2026-01-12 15:36:10 +0800

    Add Google Analytics 4

--- js/init.js
+++ js/init.js
@@ -22,4 +22,18 @@
         adsense.crossOrigin = 'anonymous';
         document.head.appendChild(adsense);
     }
+
+    // Add Google Analytics 4 if not present
+    if (!document.querySelector('script[src*="googletagmanager"]')) {
+        const ga = document.createElement('script');
+        ga.async = true;
+        ga.src = 'https://www.googletagmanager.com/gtag/js?id=G-QFTQMV9QP7';
+        document.head.appendChild(ga);
+
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        window.gtag = gtag;
+        gtag('js', new Date());
+        gtag('config', 'G-QFTQMV9QP7');
+    }
 })();
